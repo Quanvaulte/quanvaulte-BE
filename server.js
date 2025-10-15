dfe872be1e568dbb87d3115ea24908389e94544d
clement <clemcy9@gmail.com>
2025-10-15 20:07:25 +0100

    feat:added the server and configured api documentations

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { swaggerUi, swaggerSpec } from "./swagger.js";
+import connectDb from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDb();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// authentication routes
+app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log("server running on port", process.env.PORT)
+);
+
+// // running on local ip
+// app.listen(5000, "0.0.0.0", () => console.log("running on local ip"));
---
+++ swagger.js
@@ -0,0 +1,40 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Quanvaulte API",
+      version: "1.0.0",
+      description: "API documentation for Quanvault project",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000", // adjust for prod
+      },
+      {
+        url: "https://iot-server-9u9o.onrender.com", // adjust for prod
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // adjust path to where your routes are
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export { swaggerUi, swaggerSpec };
